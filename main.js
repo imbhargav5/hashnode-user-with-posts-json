4149729b105f34724b92ce5250e9a5ed6104048e
imbhargav5 <imbhargav5@gmail.com>
2021-02-02 16:28:58 +0530

    Update fields

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-const { gql, GraphQLClient } =  require('graphql-request')
+const { gql, GraphQLClient } = require("graphql-request");
 
-
-const client = new GraphQLClient('https://api.hashnode.com')
+const client = new GraphQLClient("https://api.hashnode.com");
 
 const query = gql`
-   query getUserPosts($username: String!) {
+  query getUserPosts($username: String!) {
     user(username: $username) {
       name
+      username
+      photo
+      coverImage
       numFollowers
       numReactions
       publicationDomain
-      publication{
-          posts{
-              title
-              slug
-              
-          }
+      publication {
+        posts {
+          title
+          coverImage
+          brief
+          cuid
+          bookmarkedIn
+        }
       }
     }
   }
-`
+`;
 
-async function getHashnodePosts(username){
-    try{
-        const data = await client.request(query, {username})
-        return data;
-    }catch(err){
-        console.log(err)
-        return {}
-    }
+async function getHashnodePosts(username) {
+  try {
+    const data = await client.request(query, { username });
+    return data;
+  } catch (err) {
+    console.log(err);
+    return {};
+  }
 }
 
-module.exports = getHashnodePosts
+module.exports = getHashnodePosts;
